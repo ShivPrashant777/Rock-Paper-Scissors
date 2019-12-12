b2f5b295867c43c43ad9c3cd49023bea8f302561
ShivPrashant777 <shivprashant777@gmail.com>
2019-12-12 19:35:27 +0530

    First Commit
    Rules Window Added

---
+++ js/close.js
@@ -0,0 +1,6 @@
+var close = document.getElementById('close');
+var rules_window = document.getElementById('rules');
+
+close.addEventListener('click', () => {
+    rules_window.style.display = 'none';
+})
