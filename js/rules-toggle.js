c2d72d70bc818f57ffc0de04c2e9fcd38ce83bb6
ShivPrashant777 <shivprashant777@gmail.com>
2019-12-12 23:53:42 +0530

    Third Commit
     Rules Toggle Added

--- js/rules-toggle.js
+++ js/rules-toggle.js
@@ -1,6 +1,12 @@
 var close = document.getElementById('close');
 var rules_window = document.getElementById('rules');
 
+var open = document.getElementById('rules-button');
+
 close.addEventListener('click', () => {
     rules_window.style.display = 'none';
+})
+
+open.addEventListener('click', () => {
+    rules_window.style.display = 'block';
 })
