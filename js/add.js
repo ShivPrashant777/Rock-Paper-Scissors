45116a49e67b55a84d51d560c935b4defefda08c
ShivPrashant777 <shivprashant777@gmail.com>
2019-12-13 19:05:37 +0530

    Seventh Commit
    CPU Icons

--- js/add.js
+++ js/add.js
@@ -3,9 +3,13 @@ var rock = document.querySelector("#rock");
 var paper = document.querySelector("#paper");
 var scissor = document.querySelector("#scissors");
 var tri = document.querySelector("#tri");
+var c_rock = document.querySelector(".c-rock"); 
+var c_paper = document.querySelector(".c-paper"); 
+var c_scissor = document.querySelector(".c-scisscors"); 
 var game = [rock , paper , scissor];
 var score = 0;
 var scoreDisplay = document.querySelector("#score-count");
+var replay = document.querySelector('#replay');
 
 var main = document.querySelector(".game");
 
@@ -17,6 +21,21 @@ game.forEach(function(uP,uPick){
         console.log(uPick,cPick);
         gameController(uPick,cPick);
         });
+});
+
+replay.addEventListener('click', () => {
+    paper.style.display = "block";
+    scissor.style.display = "block";
+    tri.style.display = "block";
+    rock.style.display = "block";
+    rock.style.bottom = '460px';
+    rock.style.left = '125px';
+    paper.style.bottom = '380px';
+    scissor.style.bottom = '530px';
+    scissor.style.left = '250px';
+    c_paper.style.display = "none";
+    c_scissor.style.display = "none";
+    c_rock.style.display = "none";
 });
 
 function gameController(uPick,cPick){
@@ -31,14 +50,17 @@ function gameController(uPick,cPick){
         rock.style.left = 0;
 
         if(cPick === 1){
+            c_paper.style.display = "block";
             console.log("You Lost");
             score--;
         }
         else if(cPick === 2){
+            c_scissor.style.display = "block";
             console.log("You Win");
             score++;
            
         }else if(cPick === 0){
+            c_paper.style.display = "block";
             console.log("Tie");
            
         }
@@ -53,15 +75,18 @@ function gameController(uPick,cPick){
         paper.style.left = 0;
 
         if(cPick === 2){
+            c_scissor.style.display = "block";
             console.log("You Lost");
             score--;
             
         }
         else if(cPick === 0){
+            c_paper.style.display = "block";
             console.log("You Win");
             score++;
            
         }else if(cPick === 1){
+            c_paper.style.display = "block";
             console.log("Tie");
             
         }
@@ -76,15 +101,18 @@ function gameController(uPick,cPick){
         scissor.style.left = 0;
 
         if(cPick === 0){
+            c_paper.style.display = "block";
             console.log("You Lost");
             score--;
             
         }
         else if(cPick === 1){
+            c_paper.style.display = "block";
             console.log("You Win");
             score++;
             
         }else if(cPick === 2){
+            c_scissor.style.display = "block";
             console.log("Tie");
             
         }
