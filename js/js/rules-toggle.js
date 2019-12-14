174244ee8ca2a8c20333d099e9cd38534120d491
Nikhil Chacko <55291138+Nikhil-2019@users.noreply.github.com>
2019-12-14 09:45:15 +0530

    Tenth Commit

---
+++ js/js/rules-toggle.js
@@ -0,0 +1,16 @@
+
+var close = document.getElementById('close');
+var rules_window = document.getElementById('rules');
+
+var open = document.getElementById('rules-button');
+
+close.addEventListener('click', () => {
+    rules_window.style.display = 'none';
+});
+
+open.addEventListener('click', () => {
+    rules_window.style.display = 'block';
+});
+
+
+
